7ab77c73bded5ef798ae9baecb4f8da82f0b611d
iwenLjr <1816807688@qq.com>
2020-11-12 11:30:21 +0800

    添加依赖

--- app/src/main/java/com/example/iwen/imqingliao/MainActivity.java
+++ app/src/main/java/com/example/iwen/imqingliao/MainActivity.java
@@ -1,27 +1,22 @@
 package com.example.iwen.imqingliao;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.iwen.common.Common;
 import com.example.iwen.common.app.Activity;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
-public class MainActivity extends Activity {
-    @BindView(R.id.txt_test)
-    TextView mTextView;
-
+public class MainActivity extends Activity{
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_main;
     }
 
     @Override
-    protected void initWidget() {
-        super.initWidget();
-        mTextView.setText("QingLiao");
+    protected void initData() {
+        super.initData();
     }
 }
