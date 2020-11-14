57554b257b8b475c15b444306a15a0f92c4ab724
iwenLjr <1816807688@qq.com>
2020-11-15 02:08:59 +0800

    头像图片剪裁1

---
+++ app/src/main/java/com/example/iwen/imqingliao/App.java
@@ -0,0 +1,10 @@
+package com.example.iwen.imqingliao;
+
+import com.example.iwen.common.app.Application;
+
+/**
+ * author : Iwen大大怪
+ * create : 2020/11/15 2:05
+ */
+public class App extends Application {
+}
--- app/src/main/java/com/example/iwen/imqingliao/fragments/account/UpdateInfoFragment.java
+++ app/src/main/java/com/example/iwen/imqingliao/fragments/account/UpdateInfoFragment.java
@@ -1,9 +1,16 @@
 package com.example.iwen.imqingliao.fragments.account;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import com.example.iwen.common.app.Application;
 import com.example.iwen.common.app.Fragment;
 import com.example.iwen.common.widget.PortraitView;
 import com.example.iwen.imqingliao.R;
 import com.example.iwen.imqingliao.fragments.media.GalleryFragment;
+import com.yalantis.ucrop.UCrop;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -33,7 +40,18 @@ public class UpdateInfoFragment extends Fragment {
         new GalleryFragment().setListener(new GalleryFragment.OnSelectedListener() {
             @Override
             public void onSelectedImage(String path) {
-
+                UCrop.Options options = new UCrop.Options();
+                // 设置图片处理格式
+                options.setCompressionFormat(Bitmap.CompressFormat.JPEG);
+                // 设置压缩后图片的精度
+                options.setCompressionQuality(96);
+                // 得到头像缓存地址
+                File dPath = Application.getAvatarTempFile();
+                UCrop.of(Uri.fromFile(new File(path)),Uri.fromFile(dPath))
+                        .withAspectRatio(1,1) // 1:1比例
+                        .withMaxResultSize(520,520) // 最大尺寸
+                        .withOptions(options)
+                        .start(getActivity());
             }
         }).show(getChildFragmentManager(),GalleryFragment.class.getName());
     }
