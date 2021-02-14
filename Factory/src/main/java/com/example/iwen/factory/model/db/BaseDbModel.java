5ada98f88386afe99837f6fd7015a3c92e1d90d5
iwenLjr <1816807688@qq.com>
2021-02-14 21:59:56 +0800

    添加相关文件

---
+++ Factory/src/main/java/com/example/iwen/factory/model/db/BaseDbModel.java
@@ -0,0 +1,16 @@
+package com.example.iwen.factory.model.db;
+
+import com.example.iwen.factory.utils.DiffUiDataCallback;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+/**
+ * 我们APP中的基础的一个BaseDbModel，
+ * 基础了数据库框架DbFlow中的基础类
+ * 同时定义类我们需要的方
+ *
+ * @author iwen大大怪
+ * Create to 2021/02/14 21:52
+ */
+public abstract class BaseDbModel<Model> extends BaseModel
+        implements DiffUiDataCallback.UiDataDiffer<Model> {
+}
