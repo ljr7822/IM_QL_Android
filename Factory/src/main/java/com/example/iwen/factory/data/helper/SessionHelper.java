108e7765b80ebcb73132cb9b1d791f5bb1897ac4
iwenLjr <1816807688@qq.com>
2021-02-21 23:54:21 +0800

    完善DbHelper

---
+++ Factory/src/main/java/com/example/iwen/factory/data/helper/SessionHelper.java
@@ -0,0 +1,20 @@
+package com.example.iwen.factory.data.helper;
+
+import com.example.iwen.factory.model.db.Session;
+import com.example.iwen.factory.model.db.Session_Table;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+
+/**
+ * 会话辅助工具类
+ * @author iwen大大怪
+ * Create to 2021/02/21 23:50
+ */
+public class SessionHelper {
+    //从本地查询Session
+    public static Session findFromLocal(String id) {
+        return SQLite.select()
+                .from(Session.class)
+                .where(Session_Table.id.eq(id))
+                .querySingle();
+    }
+}
