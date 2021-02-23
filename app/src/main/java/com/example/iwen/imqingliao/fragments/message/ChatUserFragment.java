36c0536caeb3733440a5b3d646654c4bcacf4830
iwenLjr <1816807688@qq.com>
2021-02-24 01:37:14 +0800

    添加 菜单

--- app/src/main/java/com/example/iwen/imqingliao/fragments/message/ChatUserFragment.java
+++ app/src/main/java/com/example/iwen/imqingliao/fragments/message/ChatUserFragment.java
@@ -1,18 +1,24 @@
 package com.example.iwen.imqingliao.fragments.message;
 
+import android.view.MenuItem;
 import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.iwen.common.widget.PortraitView;
 import com.example.iwen.imqingliao.R;
+import com.example.iwen.imqingliao.activities.PersonalActivity;
 import com.google.android.material.appbar.AppBarLayout;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 用户聊天界面
  */
 public class ChatUserFragment extends ChatFragment {
-    //private MenuItem menuItem;
+    private MenuItem menuItem;
+
     @BindView(R.id.iv_avatar)
     PortraitView iv_avatar;
 
@@ -25,14 +31,29 @@ public class ChatUserFragment extends ChatFragment {
         return R.layout.fragment_chat_user;
     }
 
+    @Override
+    protected void initToolbar() {
+        super.initToolbar();
+        Toolbar toolbar = super.toolbar;
+        toolbar.inflateMenu(R.menu.chat_user);
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.action_person) {
+                onAvatarClick();
+            }
+            return true;
+        });
+        // 拿到菜单Icon
+        menuItem = toolbar.getMenu().findItem(R.id.action_person);
+    }
+
     // 进行高度的综合运算，透明我们的头像和Icon
     @Override
     public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
         super.onOffsetChanged(appBarLayout, verticalOffset);
         View view = iv_avatar;
-        //MenuItem item = menuItem;
-//        if (view == null || item == null)
-//            return;
+        MenuItem item = menuItem;
+        if (view == null || item == null)
+            return;
         if (verticalOffset == 0) {
             // 完全展开
             view.setVisibility(View.VISIBLE);
@@ -40,9 +61,9 @@ public class ChatUserFragment extends ChatFragment {
             view.setScaleY(1);
             view.setAlpha(1);
 
-//            // 隐藏Menu
-//            item.setVisible(false);
-//            item.getIcon().setAlpha(0);
+            // 隐藏Menu
+            item.setVisible(false);
+            item.getIcon().setAlpha(0);
 
         } else {
             // abs运算
@@ -55,9 +76,9 @@ public class ChatUserFragment extends ChatFragment {
                 view.setScaleY(0);
                 view.setAlpha(0);
 
-//                // 显示Menu
-//                item.setVisible(true);
-//                item.getIcon().setAlpha(255);
+                // 显示Menu
+                item.setVisible(true);
+                item.getIcon().setAlpha(255);
 
             } else {
                 // 中间状态
@@ -67,10 +88,16 @@ public class ChatUserFragment extends ChatFragment {
                 view.setScaleY(progress);
                 view.setAlpha(progress);
 
-//                // 和头像相反的操作
-//                item.setVisible(true);
-//                item.getIcon().setAlpha((int) (255 * (1 - progress)));
+                // 和头像相反的操作
+                item.setVisible(true);
+                item.getIcon().setAlpha((int) (255 * (1 - progress)));
             }
         }
+    }
+
+    // 头像的点击
+    @OnClick(R.id.iv_avatar)
+    void onAvatarClick() {
+        PersonalActivity.show(getContext(), mReceiverId);
     }
 }
